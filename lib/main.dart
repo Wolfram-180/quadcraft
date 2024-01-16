9c31728fe15614c215c6e41ccaaa9dec2f533bc3
Wolfram-180 <sergei.ulvis@gmail.com>
2024-01-16 09:44:38 +0300

    first build

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,8 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_flame_minecraft/main_game.dart';
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(GameWidget(game: MainGame()));
+}
