e59e798ce6e2faa036c2bd00c17bcffe38794618
Wolfram-180 <sergei.ulvis@gmail.com>
2024-01-16 13:06:16 +0300

    worlddata added

--- lib/layout/game_layout.dart
+++ lib/layout/game_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flame_minecraft/layout/controller_widget.dart';
 import 'package:flutter_flame_minecraft/main_game.dart';
 import 'package:flame/game.dart';
+import 'package:flutter_flame_minecraft/global/world_data.dart';
 
 class GameLayout extends StatelessWidget {
   const GameLayout({super.key});
@@ -10,7 +11,7 @@ class GameLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        GameWidget(game: MainGame()),
+        GameWidget(game: MainGame(worldData: WorldData())),
         const ControllerWidget(),
       ],
     );
--- lib/main_game.dart
+++ lib/main_game.dart
@@ -1,7 +1,13 @@
 import 'package:flame/game.dart';
 import 'package:flutter_flame_minecraft/components/player_component.dart';
+import 'package:flutter_flame_minecraft/global/world_data.dart';
 
 class MainGame extends FlameGame {
+  final WorldData worldData;
+
+  MainGame(
+      {super.children, super.world, super.camera, required this.worldData});
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
