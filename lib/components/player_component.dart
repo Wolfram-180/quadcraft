05cf12ef486d837ad0d4286a6aaab5ded2e6bd97
Wolfram-180 <sergei.ulvis@gmail.com>
2024-01-16 16:17:29 +0300

    moves done

--- lib/components/player_component.dart
+++ lib/components/player_component.dart
@@ -5,35 +5,75 @@ import 'package:flutter_flame_minecraft/global/global_game_reference.dart';
 import 'package:flutter_flame_minecraft/global/player_data.dart';
 
 class PlayerComponent extends SpriteAnimationComponent {
+  final Vector2 playerDimensions = Vector2.all(60);
+  final double stepTime = 0.3;
   final double speed = 5;
+  bool isFacingRight = true;
+
+  late SpriteSheet playerWalkingSpritesheet;
+  late SpriteSheet playerIdleSpritesheet;
+
+  late SpriteAnimation walkingAnimation =
+      playerWalkingSpritesheet.createAnimation(row: 0, stepTime: stepTime);
+
+  late SpriteAnimation idleAnimation =
+      playerIdleSpritesheet.createAnimation(row: 0, stepTime: stepTime);
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
-    SpriteSheet playerSpriteSheet = SpriteSheet(
+    playerWalkingSpritesheet = SpriteSheet(
       image: await Flame.images
           .load('sprite_sheets/player/player_walking_sprite_sheet.png'),
-      srcSize: Vector2(60, 60),
+      srcSize: playerDimensions,
     );
 
-    animation = playerSpriteSheet.createAnimation(row: 0, stepTime: 0.1);
-    size = Vector2(100, 100);
+    playerIdleSpritesheet = SpriteSheet(
+      image: await Flame.images
+          .load('sprite_sheets/player/player_idle_sprite_sheet.png'),
+      srcSize: playerDimensions,
+    );
+
+    size = Vector2.all(60);
     position = Vector2(100, 500);
+    animation = idleAnimation;
   }
 
   @override
   void update(double dt) {
     super.update(dt);
+    movementLogic();
+  }
+
+  void movementLogic() {
     if (GlobalGameReference
             .instance.gameReference.worldData.playerData.componentMotionState ==
         ComponentMotionState.walkingLeft) {
       position.x -= speed;
+      if (isFacingRight) {
+        flipHorizontallyAroundCenter();
+        isFacingRight = false;
+      }
+
+      animation = walkingAnimation;
     }
 
     if (GlobalGameReference
             .instance.gameReference.worldData.playerData.componentMotionState ==
         ComponentMotionState.walkingRight) {
       position.x += speed;
+      if (!isFacingRight) {
+        flipHorizontallyAroundCenter();
+        isFacingRight = true;
+      }
+      animation = walkingAnimation;
+    }
+
+    if (GlobalGameReference
+            .instance.gameReference.worldData.playerData.componentMotionState ==
+        ComponentMotionState.idle) {
+      animation = idleAnimation;
     }
   }
 }
