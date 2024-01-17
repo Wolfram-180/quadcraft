2ef309d6a730fe9999c0e525c03af85d598e6a63
Wolfram-180 <sergei.ulvis@gmail.com>
2024-01-17 13:17:08 +0300

    chunk generation

---
+++ lib/utils/chunk_generation_methods.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_flame_minecraft/utils/constants.dart';
+import 'package:flutter_flame_minecraft/resources/blocks.dart';
+
+class ChunkGenerationMethods {
+  static ChunkGenerationMethods get instance => ChunkGenerationMethods();
+
+  List<Null> getNullRow() {
+    return List.generate(chunkWidth, (index) => null);
+  }
+
+  List<List<Blocks?>> generateNullChunk() {
+    return List.generate(chunkHeight, (index) => getNullRow());
+  }
+
+  List<List<Blocks?>> generateChunk() {
+    List<List<Blocks?>> chunk = generateNullChunk();
+
+    chunk.asMap().forEach((int indOfRowOfBlocks, List<Blocks?> rowOfBlocks) {
+      if (indOfRowOfBlocks == 5) {
+        rowOfBlocks.asMap().forEach((int index, Blocks? value) {
+          chunk[5][index] = Blocks.grass;
+        });
+      }
+    });
+
+    return chunk;
+  }
+}
